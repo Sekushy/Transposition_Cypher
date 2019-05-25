1349cabeb6ef728fd707f1d522a33173acc56f19
Drăghici Andrei <35525357+Sekushy@users.noreply.github.com>
2019-05-25 08:14:45 +0300

    Finished encryption side of program

--- Cypher.py
+++ Cypher.py
@@ -9,6 +9,45 @@ def encryptMessage(message, key):
         for i in range(dummy_characters):
             message += "q"
 
+    numberOfRows = int(len(message) / len(key))
+    matrix = [[0] * len(key) for i in range(numberOfRows)]
+
+    # z is just an index
+    z = 0
+    for i in range(numberOfRows):
+        for j in range(len(key)):
+            matrix[i][j] = message[z]
+            z += 1
+
+    for i in range(numberOfRows):
+        for j in range(len(key)):
+            print(matrix[i][j], end= ' ')
+        print()
+
+    numberLocation = getLocationOfNumbersInKey(keyNumber, key)
+    print(numberLocation)
+
+    encryptedMessage = ''
+    k = 0
+    for i in range(numberOfRows):
+        if k == len(key):
+            break
+        else:
+            temp = int(numberLocation[k])
+        for j in range(numberOfRows):
+            encryptedMessage += matrix[j][temp]
+        k += 1
+    print('Encrypted message: {}'.format(encryptedMessage))
+
+def getLocationOfNumbersInKey(keyNumber, key):
+    numberLocation = ''
+    for i in range(len(key) + 1):
+        for j in range(len(key)):
+            if keyNumber[j] == i:
+                numberLocation += str(j)
+    return numberLocation
+
+
 def generateKeyNumber(key):
     keyword = list(range(len(key)))
     tempPositionerForCharInKey = 0
